650de36717db75a0a80967ef503aba509219c319
premasanub <premakumaran15@gmail.com>
2025-12-22 19:54:58 +0530

    base setup completed

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import connectDB from './DbConnection/dbConnection.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+
+//dotenv config
+dotenv.config();
+
+
+//express initialization
+const app=express();
+
+
+
+//default middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+//Database connection
+connectDB();
+
+//default route
+app.get("/",(req,res) =>{
+    res.status(200).send({message:"welcome to reset password backend"});
+});
+
+//custom routes
+
+//port
+const PORT=process.env.PORT || 3000;
+
+//server starting
+app.listen(PORT,()=>{
+    console.log ("server started");
+});
